5cd8ef0f886b95cf7dca03c9f7d2cd4df77999a9
Polina <polina.bystrevskaya@gmail.com>
2020-02-23 01:24:14 +0700

    bug fix... maybe

--- distribution_gui.py
+++ distribution_gui.py
@@ -3,6 +3,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 from tkinter import *
 import numpy as np
+from PIL import ImageTk, Image
 
 
 def dist_norm(event):
@@ -21,8 +22,8 @@ def dist_norm(event):
     my_density = gaussian_kde(mr, bw_method=.45)
     ax.plot(x, my_density(x), 'g', lw=2)
 
-    canvas = FigureCanvasTkAgg(fig, master=window)
-    canvas.get_tk_widget().grid(row=2)
+    canvas = FigureCanvasTkAgg(fig, master=graph)
+    canvas.get_tk_widget().grid(row=0)
 
 
 def dist_gamma(event):
@@ -43,8 +44,8 @@ def dist_gamma(event):
     my_density = gaussian_kde(mr, bw_method=.35)
     ax.plot(x, my_density(x), 'g', lw=2)
 
-    canvas = FigureCanvasTkAgg(fig, master=window)
-    canvas.get_tk_widget().grid(row=2)
+    canvas = FigureCanvasTkAgg(fig, master=graph)
+    canvas.get_tk_widget().grid(row=0)
 
 
 def dist_log(event):
@@ -63,8 +64,8 @@ def dist_log(event):
     my_density = gaussian_kde(mr, bw_method=.45)
     ax.plot(x, my_density(x), 'g', lw=2)
 
-    canvas = FigureCanvasTkAgg(fig, master=window)
-    canvas.get_tk_widget().grid(row=2)
+    canvas = FigureCanvasTkAgg(fig, master=graph)
+    canvas.get_tk_widget().grid(row=0)
 
 
 def dist_exp(event):
@@ -83,15 +84,15 @@ def dist_exp(event):
     my_density = gaussian_kde(mr, bw_method=.35)
     ax.plot(x, my_density(x), 'g', lw=2)
 
-    canvas = FigureCanvasTkAgg(fig, master=window)
-    canvas.get_tk_widget().grid(row=2)
+    canvas = FigureCanvasTkAgg(fig, master=graph)
+    canvas.get_tk_widget().grid(row=0)
 
 
 def dist_weibull(event):
     fig, ax = plt.subplots(1, 1)
     c = 2
     x = np.linspace(weibull_min.ppf(0.001, c),
-                    weibull_min.ppf(0.999, c), 1000)
+    weibull_min.ppf(0.999, c), 1000)
     rv = weibull_min(c)
     ax.plot(x, rv.pdf(x), 'r', lw=4, alpha=0.8)
 
@@ -105,31 +106,41 @@ def dist_weibull(event):
     my_density = gaussian_kde(mr, bw_method=.45)
     ax.plot(x, my_density(x), 'g', lw=2)
 
-    canvas = FigureCanvasTkAgg(fig, master=window)
-    canvas.get_tk_widget().grid(row=2)
-
+    canvas = FigureCanvasTkAgg(fig, master=graph)
+    canvas.get_tk_widget().grid(row=0)
 
 
 window = Tk()
-window.geometry('1200x560')
+window.geometry('600x600')
+window.title("Лабораторная работа №1")
+window.resizable(0, 0)
 
-title = Label(window, text="Вывод графиков распределений")
+graph = Toplevel(window)
+graph.title("Числа и график")
+
+hello = Label(graph)
+
+background_image = ImageTk.PhotoImage(Image.open("image.jpg"))
+background_label = Label(image=background_image)
+background_label.grid(columnspan=2, rowspan=5)
+
+title = Label(window, text="Вывод графиков распределений", bd=15)
 title.grid(row=0, columnspan=2)
 
-norm_d = Button(window, text="Нормальное распределение")
+norm_d = Button(window, text="Нормальное распределение", bd=5, width=82)
 norm_d.grid(row=1, columnspan=2)
 norm_d.bind("<Button-1>", dist_norm)
-gamma_d = Button(window, text="Гамма-распределение")
-gamma_d.grid(row=2, column=0)
+gamma_d = Button(window, text="Гамма-распределение", bd=5, width=40)
+gamma_d.grid(row=2, column=0, sticky=E)
 gamma_d.bind("<Button-1>", dist_gamma)
-log_d = Button(window, text="Логистическое распределение")
-log_d.grid(row=2, column=1)
+log_d = Button(window, text="Логистическое распределение", bd=5, width=40)
+log_d.grid(row=2, column=1, sticky=W)
 log_d.bind("<Button-1>", dist_log)
-weibull_d = Button(window, text="Распределение Вейбулла")
-weibull_d.grid(row=3, column=0)
+weibull_d = Button(window, text="Распределение Вейбулла", bd=5, width=40)
+weibull_d.grid(row=3, column=0, sticky=E)
 weibull_d.bind("<Button-1>", dist_weibull)
-exp_d = Button(window, text="Экспоненциальное распределение")
-exp_d.grid(row=3, column=1)
+exp_d = Button(window, text="Экспоненциальное распределение", bd=5, width=40)
+exp_d.grid(row=3, column=1, sticky=W)
 exp_d.bind("<Button-1>", dist_exp)
 
 window.mainloop()
