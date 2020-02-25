042f4ad1645eb6a3337d8b48ec2392cb3ed93eb5
Vyacheslav <shirkeev@example.com>
2020-02-25 23:26:59 +0700

    fixing last failure

--- distributions_gui.py
+++ distributions_gui.py
@@ -112,7 +112,7 @@ def dist_weibull(event):
 
 
 window = Tk()
-window.geometry('500x350')
+window.geometry('600x500')
 window.title("Лабораторная работа №1")
 window.resizable(0, 0)
 
@@ -130,31 +130,31 @@ background_color = "#384c6f"
 button_color = "#dabbaa"
 
 title      = Label(window, text="Вывод графиков распределений", bd=15,
-              bg=background_color, font=("Arial Black", 12))
+                   bg=background_color, font=("Arial Black", 12), fg='#ffffff')
 title.grid(row=0, columnspan=2)
 
 norm_d     = Button(window, text="Нормальное распределение", bd=5,
-                bg=button_color, heigh=3, width=38, font=("Arial Bold", 10))
+                    bg=button_color, heigh=3, width=45, font=("Arial Bold", 10))
 norm_d.grid(row=1, columnspan=2)
 norm_d.bind("<Button-1>", dist_norm)
 
 gamma_d    = Button(window, text="Гамма-распределение", bd=5,
-                 bg=button_color, heigh=3, width=18, font=("Arial Bold", 10))
+                    bg=button_color, heigh=3, width=35, font=("Arial Bold", 10))
 gamma_d.grid(row=2, column=0, sticky=E)
 gamma_d.bind("<Button-1>", dist_gamma)
 
-log_d     = Button(window, text="Логистическое\nраспределение", bd=5,
-               bg=button_color, heigh=3, width=18, font=("Arial Bold", 10))
+log_d      = Button(window, text="Логистическое\nраспределение", bd=5,
+                    bg=button_color, heigh=3, width=35, font=("Arial Bold", 10))
 log_d.grid(row=2, column=1, sticky=W)
 log_d.bind("<Button-1>", dist_log)
 
-weibull_d = Button(window, text="Распределение\nВейбулла", bd=5,
-                   bg=button_color, heigh=3, width=18, font=("Arial Bold", 10))
+weibull_d  = Button(window, text="Распределение\nВейбулла", bd=5,
+                    bg=button_color, heigh=3, width=35, font=("Arial Bold", 10))
 weibull_d.grid(row=3, column=0, sticky=E)
 weibull_d.bind("<Button-1>", dist_weibull)
 
-exp_d     = Button(window, text="Экспоненциальное\nраспределение", bd=5,
-               bg=button_color, heigh=3, width=18, font=("Arial Bold", 10))
+exp_d      = Button(window, text="Экспоненциальное\nраспределение", bd=5,
+                    bg=button_color, heigh=3, width=35, font=("Arial Bold", 10))
 exp_d.grid(row=3, column=1, sticky=W)
 exp_d.bind("<Button-1>", dist_exp)
 
