4f15f5e4c1751cf87522d6f2cff74dce45e19b84
Vyacheslav <shirkeev@example.com>
2020-03-03 22:07:03 +0700

    added abspath to image and file

--- distributions_gui.py
+++ distributions_gui.py
@@ -1,13 +1,18 @@
-from scipy.stats import norm, gamma, logistic, weibull_min, expon, trimboth, gaussian_kde, \
-                        kurtosis, skew, tstd, describe, variation
+from scipy.stats import norm, gamma, logistic, weibull_min, expon, trimboth, gaussian_kde, kurtosis, skew
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
-from tkinter import *
+from os.path import abspath
 import numpy as np
-from PIL import ImageTk, Image
+from tkinter import *
+from PIL import Image, ImageTk
 
 
-def get_mode(series: list) -> float:
+BG_COLOR   = "#fff"
+IMAGE_PATH = abspath('image.jpg')
+NUM_PATH   = abspath('dist_numbers.txt')
+
+
+def get_mode(series: np.ndarray) -> float:
     vals = {}
     mode = None
 
@@ -22,7 +27,9 @@ def get_mode(series: list) -> float:
     for k in vals.keys():
         if vals.get(k) == max_val:
             mode = k
+
     return mode
+
 
 def dist_norm(event):
     fig, ax = plt.subplots(1, 1)
@@ -31,26 +38,27 @@ def dist_norm(event):
     ax.plot(x, norm.pdf(x), 'r', lw=4, alpha=0.8)
 
     r = norm.rvs(size=200)
-    mr = trimboth(r, .025)
-    ax.hist(mr, density=True, histtype='bar', alpha=0.5, bins=20)
+    mr = trimboth(r, .05)
+    ax.hist(mr, density=True, histtype='stepfilled', alpha=0.5, bins=20)
 
     my_density = gaussian_kde(mr, bw_method=.5)
-    ax.plot(x, my_density(x), 'g', lw=2)
+    ax.plot(x, my_density(x), 'g', lw=2 )
 
     kurt, sk, md, var, std, med, avg, min, max = kurtosis(mr), skew(mr), get_mode(mr), np.var(mr), np.std(mr), \
                                                  np.median(mr), np.mean(mr), np.min(mr), np.max(mr)
-    stat = f'Kurtosis: {kurt}, Skewness: {sk}, Mode: {md}, Variance: {var}, Standart deviation: {std}, ' \
-           f'\nMedian: {med}, Mean: {avg}, Min: {min}, Max: {max}'
+    stat = f'\nKurtosis: {kurt}, Skewness: {sk}, Mode: {md}, ' \
+           f'\nVariance: {var}, Standart deviation: {std}, ' \
+           f'\nMedian: {med}, Mean: {avg}, ' \
+           f'\nMin: {min}, Max: {max}\n'
 
     canvas = FigureCanvasTkAgg(fig, master=graph)
     canvas.get_tk_widget().grid(row=0)
 
-    numbers = Label(graph, text=stat)
+    numbers = Label(graph, text=stat, font=("Arial Black", 14), bg=BG_COLOR)
     numbers.grid(row=1)
 
-    file = open('dist_numbers.txt', 'w')
-    file.write(str(mr))
-    file.close()
+    with open(NUM_PATH, 'w') as file:
+        file.write((str(mr)))
 
 
 def dist_gamma(event):
@@ -63,25 +71,26 @@ def dist_gamma(event):
 
     r = gamma.rvs(a, size=200)
     mr = trimboth(r, .025)
-    ax.hist(mr, density=True, histtype='bar', alpha=0.5, bins=20)
+    ax.hist(mr, density=True, histtype='stepfilled', alpha=0.5, bins=20)
 
     my_density = gaussian_kde(mr, bw_method=.5)
     ax.plot(x, my_density(x), 'g', lw=2)
 
     kurt, sk, md, var, std, med, avg, min, max = kurtosis(mr), skew(mr), get_mode(mr), np.var(mr), np.std(mr), \
                                                  np.median(mr), np.mean(mr), np.min(mr), np.max(mr)
-    stat = f'Kurtosis: {kurt}, Skewness: {sk}, Mode: {md}, Variance: {var}, Standart deviation: {std}, ' \
-           f'\nMedian: {med}, Mean: {avg}, Min: {min}, Max: {max}'
+    stat = f'\nKurtosis: {kurt}, Skewness: {sk}, Mode: {md}, ' \
+           f'\nVariance: {var}, Standart deviation: {std}, ' \
+           f'\nMedian: {med}, Mean: {avg}, ' \
+           f'\nMin: {min}, Max: {max}\n'
 
     canvas = FigureCanvasTkAgg(fig, master=graph)
     canvas.get_tk_widget().grid(row=0)
 
-    numbers = Label(graph, text=stat)
+    numbers = Label(graph, text=stat, font=("Arial Black", 14), bg=BG_COLOR)
     numbers.grid(row=1)
 
-    file = open('dist_numbers.txt', 'w')
-    file.write(str(mr))
-    file.close()
+    with open(NUM_PATH, 'w') as file:
+        file.write((str(mr)))
 
 
 def dist_log(event):
@@ -92,25 +101,26 @@ def dist_log(event):
 
     r = logistic.rvs(size=200)
     mr = trimboth(r, .025)
-    ax.hist(mr, density=True, histtype='bar', alpha=0.5, bins=20)
+    ax.hist(mr, density=True, histtype='stepfilled', alpha=0.5, bins=20)
 
     my_density = gaussian_kde(mr, bw_method=.45)
     ax.plot(x, my_density(x), 'g', lw=2)
 
     kurt, sk, md, var, std, med, avg, min, max = kurtosis(mr), skew(mr), get_mode(mr), np.var(mr), np.std(mr), \
                                                  np.median(mr), np.mean(mr), np.min(mr), np.max(mr)
-    stat = f'Kurtosis: {kurt}, Skewness: {sk}, Mode: {md}, Variance: {var}, Standart deviation: {std}, ' \
-           f'\nMedian: {med}, Mean: {avg}, Min: {min}, Max: {max}'
+    stat = f'\nKurtosis: {kurt}, Skewness: {sk}, Mode: {md}, ' \
+           f'\nVariance: {var}, Standart deviation: {std}, ' \
+           f'\nMedian: {med}, Mean: {avg}, ' \
+           f'\nMin: {min}, Max: {max}\n'
 
     canvas = FigureCanvasTkAgg(fig, master=graph)
     canvas.get_tk_widget().grid(row=0)
 
-    numbers = Label(graph, text=stat)
+    numbers = Label(graph, text=stat, font=("Arial Black", 14), bg=BG_COLOR)
     numbers.grid(row=1)
 
-    file = open('dist_numbers.txt', 'w')
-    file.write(str(mr))
-    file.close()
+    with open(NUM_PATH, 'w') as file:
+        file.write((str(mr)))
 
 
 def dist_exp(event):
@@ -122,25 +132,26 @@ def dist_exp(event):
     r = expon.rvs(size=200)
     mr = trimboth(r, .025)
 
-    ax.hist(mr, density=True, histtype='bar', alpha=0.5, bins=20)
+    ax.hist(mr, density=True, histtype='stepfilled', alpha=0.5, bins=20)
 
-    my_density = gaussian_kde(mr, bw_method=.35)
+    my_density = gaussian_kde(mr, bw_method=.4)
     ax.plot(x, my_density(x), 'g', lw=2)
 
     kurt, sk, md, var, std, med, avg, min, max = kurtosis(mr), skew(mr), get_mode(mr), np.var(mr), np.std(mr), \
                                                  np.median(mr), np.mean(mr), np.min(mr), np.max(mr)
-    stat = f'Kurtosis: {kurt}, Skewness: {sk}, Mode: {md}, Variance: {var}, Standart deviation: {std}, ' \
-           f'\nMedian: {med}, Mean: {avg}, Min: {min}, Max: {max}'
+    stat = f'\nKurtosis: {kurt}, Skewness: {sk}, Mode: {md}, ' \
+           f'\nVariance: {var}, Standart deviation: {std}, ' \
+           f'\nMedian: {med}, Mean: {avg}, ' \
+           f'\nMin: {min}, Max: {max}\n'
 
     canvas = FigureCanvasTkAgg(fig, master=graph)
     canvas.get_tk_widget().grid(row=0)
 
-    numbers = Label(graph, text=stat)
+    numbers = Label(graph, text=stat, font=("Arial Black", 14), bg=BG_COLOR)
     numbers.grid(row=1)
 
-    file = open('dist_numbers.txt', 'w')
-    file.write(str(mr))
-    file.close()
+    with open(NUM_PATH, 'w') as file:
+        file.write((str(mr)))
 
 
 def dist_weibull(event):
@@ -153,25 +164,26 @@ def dist_weibull(event):
 
     r = weibull_min.rvs(c, size=200)
     mr = trimboth(r, .025)
-    ax.hist(mr, density=True, histtype='bar', alpha=0.5, bins=20)
+    ax.hist(mr, density=True, histtype='stepfilled', alpha=0.5, bins=20)
 
     my_density = gaussian_kde(mr, bw_method=.5)
     ax.plot(x, my_density(x), 'g', lw=2)
 
     kurt, sk, md, var, std, med, avg, min, max = kurtosis(mr), skew(mr), get_mode(mr), np.var(mr), np.std(mr), \
                                                  np.median(mr), np.mean(mr), np.min(mr), np.max(mr)
-    stat = f'Kurtosis: {kurt}, Skewness: {sk}, Mode: {md}, Variance: {var}, Standart deviation: {std}, ' \
-           f'\nMedian: {med}, Mean: {avg}, Min: {min}, Max: {max}'
+    stat = f'\nKurtosis: {kurt}, Skewness: {sk}, Mode: {md}, ' \
+           f'\nVariance: {var}, Standart deviation: {std}, ' \
+           f'\nMedian: {med}, Mean: {avg}, ' \
+           f'\nMin: {min}, Max: {max}\n'
 
     canvas = FigureCanvasTkAgg(fig, master=graph)
     canvas.get_tk_widget().grid(row=0)
 
-    numbers = Label(graph, text=stat)
+    numbers = Label(graph, text=stat, font=("Arial Black", 14), bg=BG_COLOR)
     numbers.grid(row=1)
 
-    file = open('dist_numbers.txt', 'w')
-    file.write(str(mr))
-    file.close()
+    with open(NUM_PATH, 'w') as file:
+        file.write((str(mr)))
 
 
 window = Tk()
@@ -179,15 +191,15 @@ window.geometry('600x500')
 window.title("Лабораторная работа №1")
 window.resizable(0, 0)
 
-graph = Toplevel(window)
+graph = Toplevel(window, bg=BG_COLOR)
 graph.title("Числа и график")
 
 hello = Label(graph, text="Здесь что-то будет", bd=20)
 hello.grid(row=0)
 
-background_image = ImageTk.PhotoImage(Image.open("image.jpg"))
-background_label = Label(image=background_image)
-background_label.grid(columnspan=2, rowspan=5)
+# background_image = ImageTk.PhotoImage(Image.open(IMAGE_PATH))
+# background_label = Label(image=background_image)
+# background_label.grid(columnspan=2, rowspan=5)
 
 background_color = "#384c6f"
 button_color = "#dabbaa"
@@ -221,4 +233,6 @@ exp_d      = Button(window, text="Экспоненциальное\nраспре
 exp_d.grid(row=3, column=1, sticky=W)
 exp_d.bind("<Button-1>", dist_exp)
 
-window.mainloop()
+
+if __name__ == '__main__':
+    window.mainloop()
